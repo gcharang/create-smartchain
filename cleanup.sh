0fecded29cab1fdc35a0ab6d5a157c0f398b7c7b
gcharang <mrgcharang@gmail.com>
2019-05-21 10:37:10 +0530

    makes daemon shutdown in cleanup script better

--- cleanup.sh
+++ cleanup.sh
@@ -6,6 +6,21 @@ source data
 ./c1 stop
 ./c2 stop
 
+while ./c1 stop &>/dev/null
+do
+  echo "waiting for the first daemon to stop"
+  sleep 5
+done
+
 rm -rf /home/$USER/.komodo/$name/                                                                                                                                                           Monday 20 May 2019 05:46:17 PM IST
-rm -rf ~/coinData/$name/
+
+while ./c2 stop &>/dev/null
+do
+  echo "waiting for the second daemon to stop"
+  sleep 5
+done
+rm -rf /home/$USER/coinData/$name/
+
+echo "removed the directories: '/home/$USER/.komodo/$name/' and '/home/$USER/coinData/$name/'"
+
 rm $name*
