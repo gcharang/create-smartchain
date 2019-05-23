6729c683703b09c5d886481df7d6fc327e322a89
gcharang <mrgcharang@gmail.com>
2019-05-23 22:12:44 +0530

    massive changes to add explorer installation

--- install-explorer.sh
+++ install-explorer.sh
@@ -8,6 +8,7 @@ if [ ! -f "/home/$USER/.komodo/$name/$name.conf" ]; then
   exit 1  
 fi
 
+CUR_DIR=$(pwd)
 
 pidfile="$HOME/.komodo/$name/komodod.pid"  
 
@@ -15,15 +16,15 @@ if [ -f $pidfile ]; then
   echo "The first daemon is running, it needs to be shutdown to install the explorer"
   echo "This script will now try to shut down the daemon"
   read -p "Should we proceed? (y/n) "  answer
-  while [ "answer" != "y" ] && [ "answer" != "n" ]; do
+  while [ "$answer" != "y" ] && [ "$answer" != "n" ]; do
     echo "Please answer either 'y' for yes or 'n' for no"
     read -p "Should we proceed? (y/n) "  answer
   done
   
-  if [ "answer" -eq "n" ]; then
+  if [ "$answer" = "n" ]; then
     echo "Exiting the script, please shutdown the first daemon using './c1 stop' and try again"
     exit 1
-  elif [ "answer" -eq "y" ]; then
+  elif [ "$answer" = "y" ]; then
     echo "Proceeding to shut down the daemon and install the explorer"
     ./c1 stop
     while ./c1 stop &>/dev/null
@@ -40,9 +41,16 @@ fi
 echo "Proceeding to install the explorer"
 
 echo "Cloning the explorer installer repository"
-git clone https://github.com/gcharang/komodo-install-explorer.git explorers
+success=0
+count=1
+while [ $success -eq 0 ]; do
+  echo "[Try $count] Cloning the explorer installer repository"
+  git clone https://github.com/gcharang/komodo-install-explorer explorers && success=1 || success=0
+  sleep 4
+  count=$((count+1))
+done
 
-cd explorers
+cd $CUR_DIR/explorers
 if [ ! -d "./node_modules/bitcore-node-komodo" ]; then
   echo "Setting up the explorer directory and installing dependencies" 
   ./setup-explorer-directory.sh
@@ -58,10 +66,11 @@ cd ..
 gnome-terminal -e "bash -c \"echo '$launch -pubkey=$pubkey1 -reindex'; $srcdir/$launch -pubkey=$pubkey1 -reindex; exec bash\""
 echo "started the first daemon in a new terminal with '-reindex'"
 echo "waiting for reindexing to finish"
-tail -f /home/$USER/.komodo/$name/debug.log & | while read LOGLINE
+tail -f /home/$USER/.komodo/$name/debug.log | while read LOGLINE
 do
   [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
 done
+sleep 3
 echo "the daemon has finished reindexing; shutting down the daemon"
 ./c1 stop
 while ./c1 stop &>/dev/null
@@ -71,3 +80,4 @@ do
 done
 
 echo "Use the 'start.sh' script to start the daemons"
+echo "Execute 'start.sh explorer' script to start the daemons along with the explorer"
--- start.sh
+++ start.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+. data
+
+./c1 stop
+./c2 stop
+
+while ./c1 stop &>/dev/null
+do
+  echo "waiting for the first daemon to stop"
+  sleep 5
+done
+
+while ./c2 stop &>/dev/null
+do
+  echo "waiting for the second daemon to stop"
+  sleep 5
+done
+
+
 ./create-assetchain.sh
 
 ./c1 setgenerate true 1
 echo "mining started in the first daemon"
+
+if [ "$1" = "explorer" ]; then
+  cd explorers
+  . $name-webaccess 
+  echo "Visit http://localhost:$webport to access the explorer"
+  ./$name-explorer-start.sh
+fi 
