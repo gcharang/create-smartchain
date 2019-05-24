f167fda03009c6e540e394bc49237a26c28822b4
gcharang <mrgcharang@gmail.com>
2019-05-24 16:20:49 +0530

    updates the reindex script to shutdown daemons automatically after completion

--- reindex.sh
+++ reindex.sh
@@ -26,19 +26,71 @@ if [ $1 -eq 1 ] ; then
   echo "started the first daemon in a new terminal with '-reindex' option"
   gnome-terminal -e "bash -c \"echo 'tail -f ~/.komodo/$name/debug.log'; tail -f ~/.komodo/$name/debug.log\""
   echo "opened debug.log in a new terminal"
+  echo "waiting for reindexing to finish"
+  tail -f /home/$USER/.komodo/$name/debug.log | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
+  done
+  sleep 3
+  echo "the daemon has finished reindexing; shutting down the daemon"
+  ./c1 stop
+  while ./c1 stop &>/dev/null
+  do
+    echo "waiting for the first daemon to stop"
+    sleep 5
+  done
 elif [ $1 -eq 2 ] ; then
   gnome-terminal -e "bash -c \"echo '$launch -pubkey=$pubkey2 -reindex -datadir=$datadir/$name -addnode=localhost'; $srcdir/$launch -pubkey=$pubkey2 -reindex -datadir=$datadir/$name -addnode=localhost\""
   echo "started the second daemon in a new terminal with '-reindex' option" 
   gnome-terminal -e "bash -c \"echo 'tail -f $datadir/$name/debug.log'; tail -f $datadir/$name/debug.log\""
   echo "opened debug.log in a new terminal"
+  echo "waiting for reindexing to finish"
+  tail -f $datadir/$name/debug.log | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
+  done
+  sleep 3
+  echo "the daemon has finished reindexing; shutting down the daemon"
+  ./c2 stop
+  while ./c2 stop &>/dev/null
+  do
+    echo "waiting for the second daemon to stop"
+    sleep 5
+  done  
 else
   gnome-terminal -e "bash -c \"echo '$launch -pubkey=$pubkey1'; $srcdir/$launch -pubkey=$pubkey1\""
   echo "started the first daemon in a new terminal with '-reindex' option"
   gnome-terminal -e "bash -c \"echo 'tail -f ~/.komodo/$name/debug.log'; tail -f ~/.komodo/$name/debug.log\""
   echo "opened debug.log in a new terminal"
+  echo "waiting for reindexing to finish"
+  tail -f /home/$USER/.komodo/$name/debug.log | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
+  done
+  sleep 3
+  echo "the daemon has finished reindexing; shutting down the daemon"
+  ./c1 stop
+  while ./c1 stop &>/dev/null
+  do
+    echo "waiting for the first daemon to stop"
+    sleep 5
+  done  
   gnome-terminal -e "bash -c \"echo '$launch -pubkey=$pubkey2 -reindex -datadir=$datadir/$name -addnode=localhost'; $srcdir/$launch -pubkey=$pubkey2 -reindex -datadir=$datadir/$name -addnode=localhost\""
   echo "started the second daemon in a new terminal with '-reindex' option" 
   gnome-terminal -e "bash -c \"echo 'tail -f $datadir/$name/debug.log'; tail -f $datadir/$name/debug.log\""
   echo "opened debug.log in a new terminal"
+  echo "waiting for reindexing to finish"
+  tail -f $datadir/$name/debug.log | while read LOGLINE
+  do
+    [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
+  done
+  sleep 3
+  echo "the daemon has finished reindexing; shutting down the daemon"
+  ./c2 stop
+  while ./c2 stop &>/dev/null
+  do
+    echo "waiting for the second daemon to stop"
+    sleep 5
+  done  
 fi
   
