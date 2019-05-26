80cef92dadd4c1b0def4ef280b3eac6db39c5dd9
gcharang <mrgcharang@gmail.com>
2019-05-26 22:42:28 +0530

    makes stop script stop thoroughly

--- stop.sh
+++ stop.sh
@@ -2,5 +2,19 @@
 
 echo "Stopping the first daemon if it is running"
 ./c1 stop 2>/dev/null
+
+while ./c1 stop &>/dev/null
+do
+  echo "waiting for the first daemon to stop"
+  sleep 5
+done
+
 echo "Stopping the second daemon if it is running"
 ./c2 stop 2>/dev/null
+
+while ./c2 stop &>/dev/null
+do
+  echo "waiting for the second daemon to stop"
+  sleep 5
+done
+rm -rf /home/$USER/coinData/$name/
