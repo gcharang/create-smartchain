c35fae604ecd87bc3ed38b53ed78adb1b35c0ff4
gcharang <21151592+gcharang@users.noreply.github.com>
2024-07-19 19:58:09 +0530

    add nogui stuff

--- install-explorer-nogui.sh
+++ install-explorer-nogui.sh
@@ -5,33 +5,32 @@ source data
 if [ ! -f "/home/$USER/.komodo/$name/$name.conf" ]; then
   echo "create the assetchain with name: $name and shut down the daemons before trying to install the explorer"
   echo "Exiting"
-  exit 1  
+  exit 1
 fi
 
 CUR_DIR=$(pwd)
 
-pidfile="$HOME/.komodo/$name/komodod.pid"  
-if [ '$1' = 'force' ];then
+pidfile="$HOME/.komodo/$name/komodod.pid"
+if [ '$1' = 'force' ]; then
   ./stop.sh
 fi
 
 if [ -f $pidfile ]; then
   echo "The first daemon is running, it needs to be shutdown to install the explorer"
   echo "This script will now try to shut down the daemon"
-  read -p "Should we proceed? (y/n) "  answer
+  read -p "Should we proceed? (y/n) " answer
   while [ "$answer" != "y" ] && [ "$answer" != "n" ]; do
     echo "Please answer either 'y' for yes or 'n' for no"
-    read -p "Should we proceed? (y/n) "  answer
+    read -p "Should we proceed? (y/n) " answer
   done
-  
+
   if [ "$answer" = "n" ]; then
     echo "Exiting the script, please shutdown the first daemon using './c1 stop' and run the script 'install-explorer.sh'"
     exit 1
   elif [ "$answer" = "y" ]; then
     echo "Proceeding to shut down the daemon and install the explorer"
     ./c1 stop 2>/dev/null
-    while ./c1 stop &>/dev/null
-    do
+    while ./c1 stop &>/dev/null; do
       echo "waiting for the first daemon to stop"
       sleep 5
     done
@@ -42,15 +41,15 @@ else
 fi
 
 echo "Launching the daemons to mine a few blocks"
-./start.sh
+./start-nogui.sh
 blocks=$(./c1 getinfo | jq -r '.blocks')
 while [ $blocks -lt 5 ]; do
   sleep 5
   blocks=$(./c1 getinfo | jq -r '.blocks')
   echo "waiting for atleast 5 blocks to be mined"
-done  
+done
 echo "Stopping the daemons to install the explorer"
-./stop.sh 
+./stop.sh
 
 if [ ! -d "$CUR_DIR/explorers" ]; then
   echo "Cloning the explorer installer repository"
@@ -60,7 +59,7 @@ if [ ! -d "$CUR_DIR/explorers" ]; then
     echo "[Try $count] Cloning the explorer installer repository"
     git clone https://github.com/gcharang/komodo-install-explorer explorers && success=1 || success=0
     sleep 4
-    count=$((count+1))
+    count=$((count + 1))
   done
 else
   echo "A directory named 'explorers' already exists; assuming it is cloned from the repo: https://github.com/gcharang/komodo-install-explorer , trying to install the explorer inside it"
@@ -68,14 +67,14 @@ fi
 
 cd $CUR_DIR/explorers
 if [ ! -d "./node_modules/bitcore-node-komodo" ]; then
-  echo "Setting up the explorer directory and installing dependencies" 
+  echo "Setting up the explorer directory and installing dependencies"
   ./setup-explorer-directory.sh
 else
   echo "Looks like the initial setup of the explorers directory and installation of dependencies has been done"
 fi
 
 if [ ! -d "./$name-explorer" ]; then
-  echo "Installing the explorer for the assetchain $name" 
+  echo "Installing the explorer for the assetchain $name"
   ./install-assetchain-explorer.sh $name noweb
 else
   echo "The explorer for $name seems to be already installed"
@@ -87,18 +86,16 @@ cd $CUR_DIR
 gnome-terminal -e "bash -c \"echo '$launch -pubkey=$pubkey1 -reindex'; $srcdir/$launch -pubkey=$pubkey1 -reindex\""
 echo "started the first daemon in a new terminal with '-reindex'"
 echo "waiting for reindexing to finish"
-tail -f /home/$USER/.komodo/$name/debug.log | while read LOGLINE
-do
+tail -f /home/$USER/.komodo/$name/debug.log | while read LOGLINE; do
   [[ "${LOGLINE}" == *"Reindexing finished"* ]] && pkill -P $$ tail
 done
 sleep 3
 echo "the daemon has finished reindexing; shutting down the daemon"
 ./c1 stop
-while ./c1 stop &>/dev/null
-do
+while ./c1 stop &>/dev/null; do
   echo "waiting for the first daemon to stop"
   sleep 5
 done
 
-echo "Use the 'start.sh' script to start the daemons"
-echo "Execute 'start.sh explorer' to start the daemons along with the explorer"
+echo "Use the 'start-nogui.sh' script to start the daemons"
+echo "Execute 'start-nogui.sh explorer' to start the daemons along with the explorer"
